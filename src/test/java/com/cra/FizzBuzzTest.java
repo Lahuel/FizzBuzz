afdbbfabdf52cf94c261b4f8ecc67b8ecef742e1
Lahuel <mokmin.park@gmail.com>
2022-03-06 14:28:40 +0900

    TC#01: 입력값 3에 대해 "Fizz" 출력

--- src/test/java/com/cra/FizzBuzzTest.java
+++ src/test/java/com/cra/FizzBuzzTest.java
@@ -1,4 +1,13 @@
 package com.cra;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
+    @Test
+    void fizzForThreeTest() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        assertEquals("Fizz", fizzBuzz.convert(3));
+    }
 }
