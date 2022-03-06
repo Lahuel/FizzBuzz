a3221b692f64b63ea24dd7e7bff12f5c81901aed
Lahuel <mokmin.park@gmail.com>
2022-03-06 14:31:12 +0900

    TC#02 구현.

--- src/main/java/com/cra/FizzBuzz.java
+++ src/main/java/com/cra/FizzBuzz.java
@@ -2,6 +2,9 @@ package com.cra;
 
 public class FizzBuzz {
     public String convert(int number) {
+        if (number == 5) {
+            return "Buzz";
+        }
         return "Fizz";
     }
 }
