2724655248b72644fa6dae8653d51b98311cc53c
Lahuel <mokmin.park@gmail.com>
2022-03-06 15:22:41 +0900

    Refactoring

--- src/main/java/com/cra/FizzBuzz.java
+++ src/main/java/com/cra/FizzBuzz.java
@@ -3,22 +3,16 @@ package com.cra;
 import java.util.ArrayList;
 import java.util.List;
 
-public class FizzBuzz {
-    private static final int START_INCLUSIVE = 1;
-    private static final int END_EXCLUSIVE = 101;
-    private static final int DIVISOR_THREE = 3;
-    private static final int DIVISOR_FIVE = 5;
-    private static final String FIZZBUZZ = "FizzBuzz";
-    private static final String FIZZ = "Fizz";
-    private static final String BUZZ = "Buzz";
+import static com.cra.Constants.*;
 
+public class FizzBuzz {
     public void fizzbuzz() {
         for (String result : getList()) {
             System.out.println(result);
         }
     }
 
-    public List<String> getList() {
+    List<String> getList() {
         List<String> result = new ArrayList<>();
         for (int i = START_INCLUSIVE; i < END_EXCLUSIVE; i++) {
             result.add(convert(i));
@@ -26,7 +20,7 @@ public class FizzBuzz {
         return result;
     }
 
-    public String convert(int number) {
+    String convert(int number) {
         if (number <= 0) {
             throw new RuntimeException("Invalid Data. number:" + number);
         }
