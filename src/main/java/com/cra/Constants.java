e30bb0868106055ca18fcb140303158fd1560e3f
Lahuel <mokmin.park@gmail.com>
2022-03-06 15:22:54 +0900

    Refactoring

---
+++ src/main/java/com/cra/Constants.java
@@ -0,0 +1,13 @@
+package com.cra;
+
+public class Constants {
+    private Constants() { } // Prevents Instantiation
+
+    public static final int START_INCLUSIVE = 1;
+    public static final int END_EXCLUSIVE = 101;
+    public static final int DIVISOR_THREE = 3;
+    public static final int DIVISOR_FIVE = 5;
+    public static final String FIZZBUZZ = "FizzBuzz";
+    public static final String FIZZ = "Fizz";
+    public static final String BUZZ = "Buzz";
+}
