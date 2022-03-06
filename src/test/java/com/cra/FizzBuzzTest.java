f44ba267592ac34dfc265bf6091bf3db54acb53e
Lahuel <mokmin.park@gmail.com>
2022-03-06 14:33:00 +0900

    TC#03 입력값 3,6에 대해 "Fizz", 5,10에 대해 "Buzz", 그 외에는 {입력값}을 출력

--- src/test/java/com/cra/FizzBuzzTest.java
+++ src/test/java/com/cra/FizzBuzzTest.java
@@ -12,8 +12,30 @@ public class FizzBuzzTest {
     }
 
     @Test
+    void fizzForSixTest() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        assertEquals("Fizz", fizzBuzz.convert(6));
+    }
+
+    @Test
     void buzzForFiveTest() {
         FizzBuzz fizzBuzz = new FizzBuzz();
         assertEquals("Buzz", fizzBuzz.convert(5));
+    }
+
+    @Test
+    void buzzForTenTest() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        assertEquals("Buzz", fizzBuzz.convert(10));
+    }
+
+    @Test
+    void printTheNumberForOthersTest() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        assertEquals("1", fizzBuzz.convert(1));
+        assertEquals("2", fizzBuzz.convert(2));
+        assertEquals("7", fizzBuzz.convert(7));
+        assertEquals("11", fizzBuzz.convert(11));
+        assertEquals("13", fizzBuzz.convert(13));
     }
 }
